f85108995b45627515bb920a4a6cbb76481513fa
Dmitriy K <Luckyworlds@mail.ru>
2023-02-01 16:39:44 +0300

    s

--- src/pages/AddpostPage.jsx
+++ src/pages/AddpostPage.jsx
@@ -74,8 +74,10 @@ const AddpostPage  = () => {
 				<div className='button_sibmit'>
 
 					<button onClick={handleCreatePost} className='submit'>Добавить</button>
-
 					
+					<button onClick={handleSubmitTest} className='submit'>Тест</button>
+
+					<button onClick={handleSubmitremove} className='submit'>Очистить</button>
 
 					<button onClick={() => navigate('/')} className='submit'>Отмена</button>
 				</div>
